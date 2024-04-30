65f484100813bffb34ea52c3e2574c719841d2e3
Iswenzz <alexisnardiello@gmail.com>
2024-04-30 20:38:03 +0200

    Triplets: x86 windows

--- build.bat
+++ build.bat
@@ -5,6 +5,6 @@ echo [+] Build
 rmdir build /s /q
 mkdir build
 cd build
-cmake .. --preset windows-dx9
+cmake .. --preset x86-windows-dx9
 cmake --build . --config Release
 cmake --install . --config Release
