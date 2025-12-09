1a43f6428fb3926e3dd5adce43884e20ae41f528
Iswenzz <alexisnardiello@gmail.com>
2025-12-09 23:56:36 +0100

    Small changes

--- src/Plugins/Player/Modules/CEF.cpp
+++ src/Plugins/Player/Modules/CEF.cpp
@@ -7,7 +7,8 @@ namespace IW3SR::Addons
 	void CEF::Initialize()
 	{
 		Browser::Start();
-		Browser::SetURL("https://youtu.be/UgQFcvYg9Kk?t=90");
+		if (Browser::Active)
+			Browser::SetURL("https://youtu.be/UgQFcvYg9Kk?t=90");
 	}
 
 	void CEF::Release()
@@ -23,25 +24,7 @@ namespace IW3SR::Addons
 			return;
 
 		std::scoped_lock lock(Browser::TextureMutex);
-		Material* mtl = Material_RegisterHandle("black", 3);
-		if (!mtl)
-			return;
 
-		/*if (mtl && mtl->textureTable && mtl->textureTable->u.image)
-		{
-			auto browserTexture = reinterpret_cast<IDirect3DTexture9*>(Browser::Texture->Data);
-			auto texture = mtl->textureTable->u.image->texture.map;
-			IDirect3DSurface9* srcSurface = nullptr;
-			IDirect3DSurface9* destSurface = nullptr;
-
-			browserTexture->GetSurfaceLevel(0, &srcSurface);
-			texture->GetSurfaceLevel(0, &destSurface);
-
-			Device::D3Device->StretchRect(srcSurface, nullptr, destSurface, nullptr, D3DTEXF_LINEAR);
-
-			srcSurface->Release();
-			destSurface->Release();
-		}*/
 		Draw2D::Rect(Browser::Texture, { 0, 0 }, Window::Size);
 	}
 }
