0184d9b0bcd7da1ed3af90b69f3d14cd89189200
Alexis Nardiello <alexisnardiello@gmail.com>
2026-02-15 19:01:17 +0100

    Added client loader and changed casing and directory

--- src/Game/main.cpp
+++ src/Game/main.cpp
@@ -14,19 +14,3 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
 	}
 	return TRUE;
 }
-
-static HMODULE library =
-	LoadLibrary((std::filesystem::path(std::getenv("WINDIR")) / "System32" / "ddraw.dll").string().c_str());
-
-#undef PROXY_LIBRARY
-#define PROXY_LIBRARY library
-
-// clang-format off
-PROXY(WINAPI, HRESULT, DirectDrawCreateEx,
-	(GUID *lpGUID, LPVOID *lplpDD, REFIID iid, IUnknown *pUnkOuter),
-	(lpGUID, lplpDD, iid, pUnkOuter))
-
-PROXY(WINAPI, HRESULT, DirectDrawEnumerateExA,
-	(LPVOID lpCallback, LPVOID lpContext, DWORD dwFlags),
-	(lpCallback, lpContext, dwFlags))
-// clang-format on
---
+++ src/Loader/main.cpp
@@ -0,0 +1,41 @@
+#include <windows.h>
+#include <filesystem>
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
+{
+	static HMODULE module = nullptr;
+
+	switch (dwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		SetDllDirectory("iw3sr");
+		module = LoadLibrary("IW3SR.dll");
+		break;
+
+	case DLL_PROCESS_DETACH:
+		FreeLibrary(module);
+		break;
+	}
+	return TRUE;
+}
+
+static HMODULE library =
+	LoadLibrary((std::filesystem::path(std::getenv("WINDIR")) / "System32" / "ddraw.dll").string().c_str());
+
+#define PROXY_LIBRARY library
+#define PROXY(CallingConv, ReturnType, FuncName, Params, Args)                   \
+	__declspec(dllexport) ReturnType CallingConv _##FuncName Params              \
+	{                                                                            \
+		static auto address = GetProcAddress(PROXY_LIBRARY, #FuncName);          \
+		return reinterpret_cast<ReturnType(CallingConv *) Params>(address) Args; \
+	}
+
+// clang-format off
+PROXY(WINAPI, HRESULT, DirectDrawCreateEx,
+	(GUID *lpGUID, LPVOID *lplpDD, REFIID iid, IUnknown *pUnkOuter),
+	(lpGUID, lplpDD, iid, pUnkOuter))
+
+PROXY(WINAPI, HRESULT, DirectDrawEnumerateExA,
+	(LPVOID lpCallback, LPVOID lpContext, DWORD dwFlags),
+	(lpCallback, lpContext, dwFlags))
+// clang-format on
