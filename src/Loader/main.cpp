9514f078c6403dae11f36c123d9c14eef10ecada
Alexis Nardiello <alexisnardiello@gmail.com>
2026-02-19 21:18:42 +0100

    Environment: Added Bin folder

--- src/Loader/main.cpp
+++ src/Loader/main.cpp
@@ -8,7 +8,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
 	switch (dwReason)
 	{
 	case DLL_PROCESS_ATTACH:
-		SetDllDirectory("iw3sr");
+		SetDllDirectory("iw3sr/Bin");
 		module = LoadLibrary("IW3SR.dll");
 		break;
 
