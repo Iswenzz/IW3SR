040508549c8f1df21cebac39f05082ff69d46e9a
Alexis Nardiello <alexisnardiello@gmail.com>
2026-05-20 21:32:50 +0200

    Texts: Updated emojis

--- src/Plugins/Graphics/Modules/Texts.cpp
+++ src/Plugins/Graphics/Modules/Texts.cpp
@@ -4,56 +4,56 @@ namespace IW3SR::Addons
 {
 	Texts::Texts() : Module("sr.graphics.texts", "Graphics", "Texts")
 	{
-		UseEmojis = false;
+		UseEmojis = true;
 
 		EmojiMap = {
-			{ "afr", Texture::Load("Textures/Emojis/afr.png") },
-			{ "ang", Texture::Load("Textures/Emojis/ang.png") },
+			{ "africa", Texture::Load("Textures/Emojis/africa.png") },
+			{ "angry", Texture::Load("Textures/Emojis/angry.png") },
+			{ "bored", Texture::Load("Textures/Emojis/bored.png") },
 			{ "cash", Texture::Load("Textures/Emojis/cash.png") },
 			{ "clown", Texture::Load("Textures/Emojis/clown.png") },
 			{ "cold", Texture::Load("Textures/Emojis/cold.png") },
-			{ "cow", Texture::Load("Textures/Emojis/cow.png") },
+			{ "cowboy", Texture::Load("Textures/Emojis/cowboy.png") },
+			{ "down", Texture::Load("Textures/Emojis/down.png") },
 			{ "feet", Texture::Load("Textures/Emojis/feet.png") },
 			{ "fire", Texture::Load("Textures/Emojis/fire.png") },
+			{ "happy", Texture::Load("Textures/Emojis/happy.png") },
 			{ "heart", Texture::Load("Textures/Emojis/heart.png") },
+			{ "huff", Texture::Load("Textures/Emojis/huff.png") },
+			{ "huh", Texture::Load("Textures/Emojis/huh.png") },
 			{ "imp", Texture::Load("Textures/Emojis/imp.png") },
 			{ "joy", Texture::Load("Textures/Emojis/joy.png") },
-			{ "mon", Texture::Load("Textures/Emojis/mon.png") },
-			{ "neu", Texture::Load("Textures/Emojis/neu.png") },
-			{ "noev", Texture::Load("Textures/Emojis/noev.png") },
-			{ "om", Texture::Load("Textures/Emojis/om.png") },
-			{ "pens", Texture::Load("Textures/Emojis/pens.png") },
+			{ "monkey", Texture::Load("Textures/Emojis/monkey.png") },
+			{ "neutral", Texture::Load("Textures/Emojis/neutral.png") },
+			{ "noevil", Texture::Load("Textures/Emojis/noevil.png") },
+			{ "omg", Texture::Load("Textures/Emojis/omg.png") },
+			{ "palm", Texture::Load("Textures/Emojis/palm.png") },
+			{ "pensive", Texture::Load("Textures/Emojis/pensive.png") },
 			{ "pls", Texture::Load("Textures/Emojis/pls.png") },
 			{ "rage", Texture::Load("Textures/Emojis/rage.png") },
 			{ "rofl", Texture::Load("Textures/Emojis/rofl.png") },
 			{ "rose", Texture::Load("Textures/Emojis/rose.png") },
 			{ "sad", Texture::Load("Textures/Emojis/sad.png") },
-			{ "sbo", Texture::Load("Textures/Emojis/sbo.png") },
 			{ "sh", Texture::Load("Textures/Emojis/sh.png") },
-			{ "sha", Texture::Load("Textures/Emojis/sha.png") },
 			{ "shit", Texture::Load("Textures/Emojis/shit.png") },
-			{ "sku", Texture::Load("Textures/Emojis/sku.png") },
-			{ "skuv", Texture::Load("Textures/Emojis/skuv.png") },
-			{ "skuw", Texture::Load("Textures/Emojis/skuw.png") },
-			{ "spa", Texture::Load("Textures/Emojis/spa.png") },
-			{ "sur", Texture::Load("Textures/Emojis/sur.png") },
+			{ "skull", Texture::Load("Textures/Emojis/skull.png") },
+			{ "skull2", Texture::Load("Textures/Emojis/skull2.png") },
+			{ "skull3", Texture::Load("Textures/Emojis/skull3.png") },
 			{ "sus", Texture::Load("Textures/Emojis/sus.png") },
-			{ "tdown", Texture::Load("Textures/Emojis/tdown.png") },
 			{ "think", Texture::Load("Textures/Emojis/think.png") },
-			{ "tri", Texture::Load("Textures/Emojis/tri.png") },
-			{ "tup", Texture::Load("Textures/Emojis/tup.png") },
 			{ "uk", Texture::Load("Textures/Emojis/uk.png") },
-			{ "wat", Texture::Load("Textures/Emojis/wat.png") },
+			{ "up", Texture::Load("Textures/Emojis/up.png") },
 			{ "weary", Texture::Load("Textures/Emojis/weary.png") },
-			{ "wiz", Texture::Load("Textures/Emojis/wiz.png") },
+			{ "wizard", Texture::Load("Textures/Emojis/wizard.png") },
 			{ "wtf", Texture::Load("Textures/Emojis/wtf.png") },
+			{ "wtf2", Texture::Load("Textures/Emojis/wtf2.png") },
 			{ "yawn", Texture::Load("Textures/Emojis/yawn.png") },
 		};
 	}
 
 	void Texts::Menu()
 	{
-		ImGui::Checkbox("Emojis", &UseEmojis);
+		ImGui::Checkbox("Show Emojis", &UseEmojis);
 	}
 
 	void Texts::ProcessText(std::string& text, Font_s* font, vec2 position, vec2 scale, const vec4& color)
