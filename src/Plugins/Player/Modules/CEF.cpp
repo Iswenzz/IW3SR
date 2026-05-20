adce03b01bd404671342c87ebd808f9f3e675b4a
Alexis Nardiello <alexisnardiello@gmail.com>
2026-05-20 13:50:41 +0200

    Updated engine

--- src/Plugins/Player/Modules/CEF.cpp
+++ src/Plugins/Player/Modules/CEF.cpp
@@ -10,8 +10,8 @@ namespace IW3SR::Addons
 
 	void CEF::Initialize()
 	{
-		MenuFrame.Position = { 20, 20 };
-		MenuFrame.Size = { 500, 300 };
+		MenuFrame.Position = Browser::MenuPosition;
+		MenuFrame.Size = Browser::MenuSize;
 
 		Browser::Start();
 
@@ -26,73 +26,7 @@ namespace IW3SR::Addons
 
 	void CEF::Menu()
 	{
-		if (!Browser::Open || !Browser::Texture)
-			return;
-
-		std::scoped_lock lock(Browser::TextureMutex);
-		Draw2D::DrawQuad(vec3(MenuFrame.RenderPosition, 0), MenuFrame.RenderSize, 0, Browser::Texture, vec4(1));
-		auto host = Browser::Instance->GetHost();
-
-		uint32_t modifiers = 0;
-		if (Input::IsDown(Key_Ctrl) || Input::IsDown(Key_RightCtrl))
-			modifiers |= EVENTFLAG_CONTROL_DOWN;
-		if (Input::IsDown(Key_Shift) || Input::IsDown(Key_RightShift))
-			modifiers |= EVENTFLAG_SHIFT_DOWN;
-		if (Input::IsDown(Key_Alt) || Input::IsDown(Key_RightAlt))
-			modifiers |= EVENTFLAG_ALT_DOWN;
-
-		vec2 relative = Mouse::Position - MenuFrame.RenderPosition;
-		if (Math::Contains(relative, MenuFrame.RenderSize))
-		{
-			CefMouseEvent mouseEvent;
-			mouseEvent.x = static_cast<int>(relative.x / MenuFrame.RenderSize.x * Browser::Size.x);
-			mouseEvent.y = static_cast<int>(relative.y / MenuFrame.RenderSize.y * Browser::Size.y);
-			mouseEvent.modifiers = modifiers;
-
-			host->SendMouseMoveEvent(mouseEvent, false);
-
-			if (Input::IsDown(Button_Left))
-				host->SendMouseClickEvent(mouseEvent, MBT_LEFT, false, 1);
-			if (Input::IsUp(Button_Left))
-				host->SendMouseClickEvent(mouseEvent, MBT_LEFT, true, 1);
-			if (Input::IsDown(Button_Right))
-				host->SendMouseClickEvent(mouseEvent, MBT_RIGHT, false, 1);
-			if (Input::IsUp(Button_Right))
-				host->SendMouseClickEvent(mouseEvent, MBT_RIGHT, true, 1);
-			if (Mouse::ScrollDelta)
-				host->SendMouseWheelEvent(mouseEvent, 0, Mouse::ScrollDelta * 120);
-		}
-		for (auto& [id, info] : Input::Inputs)
-		{
-			if (id == Button_Left || id == Button_Right || id == Button_Middle)
-				continue;
-			if (!Input::IsDown(id) && !Input::IsUp(id))
-				continue;
-
-			CefKeyEvent keyEvent;
-			keyEvent.modifiers = modifiers;
-			keyEvent.windows_key_code = info.OS;
-			keyEvent.native_key_code = info.OS;
-
-			if (Input::IsPressed(id))
-			{
-				keyEvent.type = KEYEVENT_RAWKEYDOWN;
-				host->SendKeyEvent(keyEvent);
-
-				if (Keyboard::Char)
-				{
-					keyEvent.type = KEYEVENT_CHAR;
-					keyEvent.windows_key_code = Keyboard::Char;
-					keyEvent.character = Keyboard::Char;
-					host->SendKeyEvent(keyEvent);
-				}
-			}
-			if (Input::IsUp(id))
-			{
-				keyEvent.type = KEYEVENT_KEYUP;
-				host->SendKeyEvent(keyEvent);
-			}
-		}
+		Browser::Frame();
 	}
 
 	void CEF::OnExecuteCommand(EventClientCommand& event)
